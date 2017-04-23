74442e6e368e0d57cfa560c1ea67e8866b3145a6
NancyYixuanXiao <yxxiao@bu.edu>
2017-04-22 20:46:09 -0400

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -1,27 +1,16 @@
 package waze;
 
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		WazeApp waze = new WazeApp ();
 		Scanner scan = new Scanner (System.in);
 		System.out.println("Please input the start node Id (only integer allowed): ");
 		int n1 = scan.nextInt();
 		System.out.println("Please input the end node Id (only integer allowed): ");
 		int n2 = scan.nextInt();
-		System.out.println("Please enter the number of shortest path (only integer allowed): ");
-		int k = scan.nextInt();
-		String path = "";
-		try {
-			path = WazeApp.demo(n1, n2, k);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(path);
+		
+		System.out.println("The paths are: ");
+		System.out.println(WazeApp.waze(n1, n2, k));
 	}
 }
