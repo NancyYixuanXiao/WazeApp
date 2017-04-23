dc6a1abf32109e350dab935243367b587d873e92
NancyYixuanXiao <yxxiao@bu.edu>
2017-04-22 23:21:18 -0400

    Update Path.java
    Path is consisting of a list of nodes and a map of distances from source to each node

--- src/Path.java
+++ src/Path.java
@@ -1,32 +1,44 @@
 package waze;
-/*
- * Define a class Path, which is a path in graph consisting of an ArrayList of edges.
- */
-import java.util.ArrayList;
+
+import java.util.*;
+
+// define the path class
 
 public class Path {
-
-	private ArrayList<Edge> edgeList;
-	private double length;
 	
-	//Constructor
-	public Path(ArrayList<Edge> edgeList){
-		this.edgeList=edgeList;
-	}
-
-	public ArrayList<Edge> getEdgeList() {
-		return edgeList;
-	}
-	public void setEdgeList(ArrayList<Edge> edgeList) {
-		this.edgeList = edgeList;
+	public List<Node> path;
+	public Map<Node, Double> lengths; // distance from source to each node in the graph
+	
+	public Path(List<Node> nodelist, Map<Node, Double> lengths) {
+		path = new ArrayList<>(nodelist);
+		this.lengths = new HashMap<>(lengths);
 	}
 	
-	public double getLength(){
-		double length=0;
-		for (int i=0;i<edgeList.size();i++){
-			length=length+edgeList.get(i).getDistance();
+	public Path(Node source, Node sink, Map<Node, Double> lengths) {
+		this.lengths = new HashMap<>(lengths);
+		path = new ArrayList<>();
+		Node curr = sink;
+		while (curr.getPrevious() != null && curr != source) {
+			path.add(curr);
+			curr = curr.getPrevious();
 		}
-		return length;
+		if (curr == source) {
+			path.add(source);
+		} else {
+			path = null;
+		}
+	}
+	
+	public double getDistance(Node node) {
+		return lengths.get(node);
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (Node node : this.path) {
+			sb.append(node.getNodeId() + " ");
+		}
+		System.out.println(sb.toString());
+		return sb.toString();
 	}
 }
-
