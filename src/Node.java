9643042962ca7c82488d48ccdbe82e9befd264f9
NancyYixuanXiao <yxxiao@bu.edu>
2017-04-22 23:19:45 -0400

    Update Node.java
    Make the class more clear and easy

--- src/Node.java
+++ src/Node.java
@@ -1,57 +1,25 @@
 package waze;
 
-/*
- * Define a class of Node. Each node has node Id, latitude and longitude.
- * It is used to construct a graph.
- */
-
+// define the Node class
 public class Node {
-
+	
 	private int nodeId;
-	private double lat;
-	private double lng;
 	private Node previous;
 	
-	//Constructor
-	public Node(int nodeId, double lat, double lng){
-		this.nodeId=nodeId;
-		this.lat=lat;
-		this.lng=lng;
-	}
-	
-	//getters and setters
-	public int getNodeId() {
-		return nodeId;
-	}
-
-	public void setNodeId(int nodeId) {
+	public Node(int nodeId) {
 		this.nodeId = nodeId;
 	}
 
-	public double getLat() {
-		return lat;
-	}
 
-	public void setLat(double lat) {
-		this.lat = lat;
-	}
-
-	public double getLng() {
-		return lng;
-	}
-
-	public void setLng(double lng) {
-		this.lng = lng;
+	public int getNodeId() {
+		return this.nodeId;
 	}
 	
-	//set a pointer points to the previous node of current node in path
+	public void setPrevious(Node node) {
+		this.previous = node;
+	}
+	
 	public Node getPrevious() {
-		return previous;
+		return this.previous;
 	}
-
-	public void setPrevious(Node previous) {
-		this.previous = previous;
-	}
-
 }
-
